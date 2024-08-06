8712ab5e998cb3ed53f1b64e7bf1805405ba37b3
Silvana <silvifarias17@gmail.com>
2024-08-06 16:34:26 -0300

    feat: funcion de leer un archivo csv e insertar sus datos en una tabla

--- main.py
+++ main.py
@@ -1,77 +1,107 @@
 import sys
 import MySQLdb
 import pandas as pd
-
-try:
-    db = MySQLdb.connect("localhost","root","","companydata" )
-except MySQLdb.Error as e:
-    print("No se pudo conectar a la base de datos: ", e)
-    sys.exit(1) 
-print("Conexión correcta.")
+import csv
 
 
-cursor = db.cursor()
+# conexión a la base de datos
+def connect_data_base():
+    try:
+        db = MySQLdb.connect("localhost","root","","companydata" )
+    except MySQLdb.Error as e:
+        print("No se pudo conectar a la base de datos: ", e)
+        sys.exit(1) 
+    print("Conexión correcta.")
+    return db
+    
+    
+# creación de tabla en la base de datos
+def creacion_tabla(cursor, tabla, columnas):
+    cursor.execute(f'DROP TABLE IF EXISTS {tabla}')
+    cursor.execute(f"CREATE TABLE {tabla} ({', '.join(columnas)})")
+    if(cursor):
+        print(f'Se ha creado la tabla {tabla}')
 
 
-def estadistica_por_departamento():
-    consulta = 'SELECT * FROM employeeperformance'
-    lec_consulta = pd.read_sql(consulta, con=db)
-    df = pd.DataFrame(lec_consulta)
+tabla_empl = 'employeeperformance'
+columnas = [
+    'id INT PRIMARY KEY AUTO_INCREMENT',
+    'department VARCHAR(255)',
+    'performance_score FLOAT',
+    'years_with_company INT',
+    'salary FLOAT'
+]
+
+# cursor 
+db = connect_data_base()
+cursor = db.cursor()  
+creacion_tabla(cursor, tabla_empl, columnas)
+
+
+# inserción de datos a la tabla
+def insertar_datos():
+    with open('employeeperformance.csv', mode='r', encoding='utf-8') as csv_file:
+        lector = csv.reader(csv_file, delimiter=',', quotechar='"')
+        next(lector)
+        try:
+            for row in lector:
+                cursor.execute(f"INSERT INTO {tabla_empl} (id, department, performance_score, years_with_company, salary) VALUES ( %s, %s, %s, %s, %s )", row[0:5])
+        except csv.Error:
+            print('Ha ocurrido un error al insertar los datos')
+            sys.exit()
+    try:
+        db.commit()
+        print('Datos insertados correctamente.')
+        print("filas insertadas:", cursor.execute("SELECT * FROM employeeperformance"))
+    except:
+        db.rollback()
+        
+
+insertar_datos()
+
+
+def estadistica_por_departamento(db, tabla):
+    
+    consulta = f'SELECT * FROM {tabla}'
+    df = pd.read_sql(consulta, db) # ejecuta una consulta sql y carga el resultado en un DataFrame directamente
+    return df
+
+df = estadistica_por_departamento(db, tabla_empl)
+print(df)
+    
     
     # agrupamos por departamento y calculamos estadisticas de performance score
-    stats_perf_score = df.groupby('department')['performance_score'].agg(
-        media='mean',
-        mediana='median',
-        desviacion_estandar='std'
-    ).reset_index()
+    # stats_perf_score = df.groupby('department')['performance_score'].agg(
+    #     media='mean',
+    #     mediana='median',
+    #     desviacion_estandar='std'
+    # ).reset_index()
     
     
-    print("Estadísticas de performance_score por departamento:")
-    print(stats_perf_score)
+    # print("Estadísticas de performance_score por departamento:")
+    # print(stats_perf_score)
     
     
     # estadisticas de salary
-    stats_salary = df.groupby('department')['salary'].agg(
-        media='mean',
-        mediana='median',
-        desviacion_estandar='std'
-    ).reset_index()
+    # stats_salary = df.groupby('department')['salary'].agg(
+    #     media='mean',
+    #     mediana='median',
+    #     desviacion_estandar='std'
+    # ).reset_index()
     
-    print("\nEstadísticas de salary por departamento:")
-    print(stats_salary)
+    # print("\nEstadísticas de salary por departamento:")
+    # print(stats_salary)
     
     
     # calcula el número total de empleados
-    total_empleados = df.groupby('department')['id'].count().reset_index()
-    total_empleados.columns = ['department', 'total_empleados']
+    # total_empleados = df.groupby('department')['id'].count().reset_index()
+    # total_empleados.columns = ['department', 'total_empleados']
     
-    print("\nNúmero total de empleados por departamento:")
-    print(total_empleados)
+    # print("\nNúmero total de empleados por departamento:")
+    # print(total_empleados)
 
-estadistica_por_departamento()
+# estadistica_por_departamento()
+
+
+cursor.close()
 db.close()
-
-
-    # consulta = "SELECT * FROM employeeperformance"
-    # lec_consulta = pd.read_sql(consulta, con=db)
-    # df = pd.DataFrame(lec_consulta)
-
-
-# Creación de la tabla
-# tabla_empleados = 'EmployeePerformance'
-# columnas = [
-#     'id INT AUTO_INCREMENT PRIMARY KEY',
-#     'deparment VARCHAR(50)',
-#     'performance_score DECIMAL',
-#     'years_with_company INT',
-#     'salary DECIMAL'
-# ]
-
-# def create_table(tabla, columnas):
-#     cursor.execute(f"DROP TABLE IF EXISTS {tabla}") # Elimina la tabla si ya existe
-#     cursor.execute(f"CREATE TABLE {tabla} ({', '.join(columnas)})") # Crea la tabla
-#     if cursor:
-#         print(f"Se ha creado la tabla {tabla}")
-
-
-# create_table(tabla_empleados, columnas)
